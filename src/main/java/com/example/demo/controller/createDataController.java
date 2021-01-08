dd7ae5eb48ec6c9669494ff560d80aaaab7fdd8c
ych <1961857865@qq.com>
2021-01-08 13:35:38 +0800

    jenkins测试

--- src/main/java/com/example/demo/controller/createDataController.java
+++ src/main/java/com/example/demo/controller/createDataController.java
@@ -1,13 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.util.createData;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class createDataController {
 
-    @PostMapping("/create")
+    @GetMapping("/create")
     public String createData(){
        return "jenkins配置成功!";
     }
